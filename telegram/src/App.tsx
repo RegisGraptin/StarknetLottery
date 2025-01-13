baeb22c57a98ce9cb8164c91982d895e0c8ade16
rere <regis-graptin@sfr.fr>
2025-01-13 14:43:30 +0700

    wip

--- telegram/src/App.tsx
+++ telegram/src/App.tsx
@@ -1,41 +1,118 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import twaLogo from './assets/tapps.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import WebApp from '@twa-dev/sdk'
+
+import { ArgentTMA, SessionAccountInterface } from '@argent/tma-wallet'
+
+// FIXME: create lib file
+const argentTMA = ArgentTMA.init({
+  environment: "sepolia", 
+  appName: "SethLottery",
+  appTelegramUrl: import.meta.env.VITE_TELEGRAM_URL,
+  sessionParams: {
+    allowedMethods: [
+      // List of contracts/methods allowed to be called by the session key
+      {
+        contract: "contracts address",
+        selector: "function name",
+      }
+    ],
+    validityDays: 90 // session validity (in days) - default: 90
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [account, setAccount] = useState<SessionAccountInterface | undefined>();
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+
+  useEffect(() => {
+
+    // Call connect() as soon as the app is loaded
+    argentTMA
+      .connect()
+      .then((res) => {
+        if (!res) {
+          // Not connected
+          setIsConnected(false);
+          return;
+        }
+
+        // Connected
+        const { account, callbackData } = res;
+
+        if (account.getSessionStatus() !== "VALID") {
+          // Session has expired or scope (allowed methods) has changed
+          // A new connection request should be triggered
+          
+          // The account object is still available to get access to user's address
+          // but transactions can't be executed
+          const { account } = res;
+
+          setAccount(account);
+          setIsConnected(false);
+          return;
+        }
+
+        // Connected
+        // const { account, callbackData } = res;
+        // The session account is returned and can be used to submit transactions
+        setAccount(account);
+        setIsConnected(true);
+        // Custom data passed to the requestConnection() method is available here
+        console.log("callback data:", callbackData);
+      })
+      .catch((err) => {
+        console.error("Failed to connect", err);
+      });
+  }, []);
+
+  const handleConnectButton = async () => {
+    // If not connected, trigger a connection request
+    // It will open the wallet and ask the user to approve the connection
+    // The wallet will redirect back to the app and the account will be available
+    // from the connect() method -- see above
+    await argentTMA.requestConnection({});
+  };
+
+  // useful for debugging
+  const handleClearSessionButton = async () => {
+    await argentTMA.clearSession();
+    setAccount(undefined);
+  };
+
+  console.log(account)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-auto">
+        {/* Navbar */}
+      <div className="bg-gray-700 p-4 flex ">
+        {!isConnected && <button className="py-2 px-4 rounded-lg bg-slate-300 text-gray-700" onClick={handleConnectButton}>Connect</button>}
+
+        {isConnected && (
+          <div className="flex justify-between items-center w-full">
+            <p className="text-green-500 text-[12px]">
+              Account address: <code>{account?.address.slice(0, 12)} ...</code>
+            </p>
+            <button className="text-sm p-2 bg-white rounded-lg bg-tomato-300 " onClick={handleClearSessionButton}>Clear Session</button>
+          </div>
+        )}
       </div>
-      <h1>TWA + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {/* outlet */}
+      <div className="grid grid-cols-1">
+        {/* {isConnected && <Game context={account}/>} */}
+
+        {isConnected && <h1>Welcome to the app</h1>}
+        {!isConnected && <h1>You need to login</h1>}
       </div>
-      {/*  */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
